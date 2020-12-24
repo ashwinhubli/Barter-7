69e86668f81e459de32917453abe3fdb3d6130c8
ashwinhubli <66745007+ashwinhubli@users.noreply.github.com>
2020-12-24 20:00:14 +0530

    Update AppDrawerNavigator.js

--- components/AppDrawerNavigator.js
+++ components/AppDrawerNavigator.js
@@ -3,6 +3,7 @@ import Settings from '../screens/Settings';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 import { AppTabNavigator } from './AppTabNavigator'
 import CustomSideBarMenu  from './CustomSideBarMenu';
+import MyBarterScreen from '../screens/MyBarters';
 
 export const AppDrawerNavigator = createDrawerNavigator({
   Home : {
@@ -10,7 +11,10 @@ export const AppDrawerNavigator = createDrawerNavigator({
     },
    Settings: {
     screen : Settings
-    }
+    },
+   MyBarters:{
+      screen : MyBarterScreen
+    },
   },
   {
     contentComponent:CustomSideBarMenu
